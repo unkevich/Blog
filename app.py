342363cdcd09fc28e5a8a820be812b5bcc86e4dd
unkevich <ilyaunkevitch@gmail.com>
2024-09-14 21:29:20 +0300

    v 0.0.2 (добавление шаблона)

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/index')
 @app.route('/')
 def index():
-    return '<h1>index</h1>'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
